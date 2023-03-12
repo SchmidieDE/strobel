3e2f268cf78639d7ffdb846e35708648c3453d47
SchmidieDE <41941167+SchmidieDE@users.noreply.github.com>
2023-03-12 21:00:37 +0100

    Datenschutz etc

--- app/pages/datenschutz.js
+++ app/pages/datenschutz.js
@@ -1,11 +1,60 @@
-
+import Link from "next/link"
+import Subtitel from "../comp/design/subtitle";
+import Subtext from "../comp/design/subtext";
+import Title from "../comp/design/title"
 
 const Datenschutz = () => {
 
 
     return (
         <>
-            Datenschutz
+        <Title text={"Datenschutz"} variant={"bigcenter"}/>
+        <Subtitel text={"Datensicherheit"} variant={"left"}/>
+        <Subtext variant="white" text={<>
+            <strong style={{fontWeight: "800"}}>Datenverarbeitung</strong><br/>
+            Die Nutzung unserer Webseite ist in der Regel ohne Angabe personenbezogener Daten möglich. Soweit auf unseren Seiten personenbezogene Daten (beispielsweise Name, Anschrift oder eMail-Adressen) erhoben werden, erfolgt dies, soweit möglich, stets auf freiwilliger Basis. Diese Daten werden ohne Ihre ausdrückliche Zustimmung nicht an Dritte weitergegeben. Wir weisen darauf hin, dass die Datenübertragung im Internet (z.B. bei der Kommunikation per E-Mail) Sicherheitslücken aufweisen kann. Ein lückenloser Schutz der Daten vor dem Zugriff durch Dritte ist nicht möglich. Der Nutzung von im Rahmen der Impressumspflicht veröffentlichten Kontaktdaten durch Dritte zur Übersendung von nicht ausdrücklich angeforderter Werbung und Informationsmaterialien wird hiermit ausdrücklich widersprochen. Die Betreiber der Seiten behalten sich ausdrücklich rechtliche Schritte im Falle der unverlangten Zusendung von Werbeinformationen, etwa durch Spam-Mails, vor.
+        </>} />
+        <Subtext variant="white" text={<>
+            <strong style={{fontWeight: "800"}}>Google Ads, Google Conversion-Linker und Google Tag Manager</strong><br/>
+            Zum Messen der Erfolge unserer Marketing-Kampagnen nutzen wir Google Ads, Google Conversion-Tracking und Google Tag Manager. Dadurch wird es uns ermöglicht, einzelne Marketing-Kampagnen besser zu bewerten. Der Google Tag Manager ist dabei nur ein Container, der an sich keine personenbezogenen Daten sammelt. Nach aktiver Zustimmung des Nutzers werden in diesen Container Google Ads und Google Conversion-Linker eingebunden. Als Parqet-Abonnent werden sowohol Google Ads, als auch der Google Conversion-Linker automatisch abgelehnt.</>} />
+        <Subtext variant="white" text={<>
+            <strong style={{fontWeight: "800"}}>Beteiligung an der Streitbeilegungsplattform der Europäischen Kommission</strong><br/>
+            Die Europäische Kommission stellt eine Plattform zur <Link style={{color: "black"}} href={"https://ec.europa.eu/consumers/odr/main/index.cfm?event=main.home.chooseLanguage"}>Online-Streitbeilegung (OS)</Link> bereit. Bei Fragen oder Problemen kannst du dich auch jederzeit bei uns melden unter Hannes-Strobel@gmx.com.
+        </>} />
+        <Subtext variant="white" text={<>
+            <strong style={{fontWeight: "800"}}>Allgemeine Hinweise zu den Rechtsgrundlagen der Datenverarbeitung auf dieser Website</strong><br/>
+            Sofern Sie in die Datenverarbeitung eingewilligt haben, verarbeiten wir Ihre personenbezogenen Daten auf Grundlage von Art. 6 Abs. 1 lit. a DSGVO bzw. Art. 9 Abs. 2 lit. a DSGVO, sofern besondere Datenkategorien nach Art. 9 Abs. 1 DSGVO verarbeitet werden. Im Falle einer ausdrücklichen Einwilligung in die Übertragung personenbezogener Daten in Drittstaaten erfolgt die Datenverarbeitung außerdem auf Grundlage von Art. 49 Abs. 1 lit. a DSGVO. Sofern Sie in die Speicherung von Cookies oder in den Zugriff auf Informationen in Ihr Endgerät (z. B. via Device-Fingerprinting) eingewilligt haben, erfolgt die Datenverarbeitung zusätzlich auf Grundlage von § 25 Abs. 1 TTDSG. Die Einwilligung ist jederzeit widerrufbar. Sind Ihre Daten zur
+
+            Vertragserfüllung oder zur Durchführung vorvertraglicher Maßnahmen erforderlich, verarbeiten wir Ihre Daten auf Grundlage des Art. 6 Abs. 1 lit. b DSGVO. Des Weiteren verarbeiten wir Ihre Daten, sofern diese zur Erfüllung einer rechtlichen Verpflichtung erforderlich sind auf Grundlage von Art. 6 Abs. 1 lit. c DSGVO. Die Datenverarbeitung kann ferner auf Grundlage unseres berechtigten Interesses nach Art. 6 Abs. 1 lit. f DSGVO erfolgen. Über die jeweils im Einzelfall einschlägigen Rechtsgrundlagen wird in den folgenden Absätzen dieser Datenschutzerklärung informiert.
+        </>} />
+        <Subtext variant="white" text={<>
+            <strong style={{fontWeight: "800"}}>Widerruf Ihrer Einwilligung zur Datenverarbeitung</strong><br/>
+            Viele Datenverarbeitungsvorgänge sind nur mit Ihrer ausdrücklichen Einwilligung möglich. Sie können eine bereits erteilte Einwilligung jederzeit widerrufen. Die Rechtmäßigkeit der bis zum Widerruf erfolgten Datenverarbeitung bleibt vom Widerruf unberührt. Widerspruchsrecht gegen die Datenerhebung in besonderen Fällen sowie gegen Direktwerbung (Art. 21 DSGVO)
+
+            Wenn die Datenverarbeitung auf Grundlage von Art. 6 Abs. 1 lit. e oder f DSGVO erfolgt, haben Sie jederzeit das Recht, aus Gründen, die sich aus Ihrer besonderen Situation ergeben, gegen die Verarbeitung Ihrer personenbezogenen Daten Widerspruch einzulegen; dies gilt auch für ein auf diese Bestimmungen gestütztes Profiling. Die jeweilige Rechtsgrundlage, auf denen eine Verarbeitung beruht, entnehmen Sie dieser Datenschutzerklärung. Wenn Sie Widerspruch einlegen, werden wir Ihre betroffenen personenbezogenen Daten nicht mehr verarbeiten, es sei denn, wir können zwingende schutzwürdige Gründe für die Verarbeitung nachweisen, die Ihre Interessen, Rechte und Freiheiten überwiegen oder die Verarbeitung dient der Geltendmachung, Ausübung oder Verteidigung von Rechtsansprüchen (Widerspruch nach Art. 21 Abs. 1 DSGVO).
+
+            Werden Ihre personenbezogenen Daten verarbeitet, um Direktwerbung zu betreiben, so haben Sie das Recht, jederzeit Widerspruch gegen die Verarbeitung Sie betreffender personenbezogener Daten zum Zwecke derartiger Werbung einzulegen; dies gilt auch für das Profiling, soweit es mit solcher Direktwerbung in Verbindung steht. Wenn Sie widersprechen, werden Ihre personenbezogenen Daten anschließend nicht mehr zum Zwecke der Direktwerbung verwendet (Widerspruch nach Art. 21 Abs. 2 DSGVO).
+        </>} />
+        <Subtext variant="white" text={<>
+            <strong style={{fontWeight: "800"}}>Beschwerderecht bei der zuständigen Aufsichtsbehörde</strong><br/>
+            Im Falle von Verstößen gegen die DSGVO steht den Betroffenen ein Beschwerderecht bei einer Aufsichtsbehörde, insbesondere in dem Mitgliedstaat ihres gewöhnlichen Aufenthalts, ihres Arbeitsplatzes oder des Orts des mutmaßlichen Verstoßes zu. Das Beschwerderecht besteht unbeschadet anderweitiger verwaltungsrechtlicher oder gerichtlicher Rechtsbehelfe.
+        </>} />
+        <Subtext variant="white" text={<>
+            <strong style={{fontWeight: "800"}}>Recht auf Datenübertragbarkeit</strong><br/>
+            Sie haben das Recht, Daten, die wir auf Grundlage Ihrer Einwilligung oder in Erfüllung eines Vertrags automatisiert verarbeiten, an sich oder an einen Dritten in einem gängigen, maschinenlesbaren Format aushändigen zu lassen. Sofern Sie die direkte Übertragung der Daten an einen anderen Verantwortlichen verlangen, erfolgt dies nur, soweit es technisch machbar ist.
+        </>} />
+        <Subtext variant="white" text={<>
+            <strong style={{fontWeight: "800"}}>Auskunft, Löschung und Berichtigung</strong><br/>
+            Sie haben im Rahmen der geltenden gesetzlichen Bestimmungen jederzeit das Recht auf unentgeltliche Auskunft über Ihre gespeicherten personenbezogenen Daten, deren Herkunft und Empfänger und den Zweck der Datenverarbeitung und ggf. ein Recht auf Berichtigung oder Löschung dieser Daten. Hierzu sowie zu weiteren Fragen zum Thema personenbezogene Daten können Sie sich jederzeit an uns wenden. Jeder Parqet-Nutzer kann im Profil die Löschung des Accounts vornehmen. Durch diese Löschung werden sämtliche Daten des Nutzers bei Parqet gelöscht, inklusive Portfolio-Daten, Aktivitäten, E-Mails, Namen und mehr. Welche Daten wir genau speichern und wofür ist unserer Datenschutz-Seite zu entnehmen. Lediglich Rechnungen (und die darauf ersichtlichen Daten) müssen wir aus rechtlichen Gründen vorhalten. Ansonsten werden automatisch sämtliche Daten gelöscht. Falls durch einen Fehler die Daten nicht vollständig gelöscht wurden, kann dies via Hannes-Strobel@gmx.com gemeldet werden.
+        </>} />
+        <Subtext variant="white" text={<>
+            <strong style={{fontWeight: "800"}}>Recht auf Einschränkung der Verarbeitung</strong><br/>
+            Sie haben das Recht, die Einschränkung der Verarbeitung Ihrer personenbezogenen Daten zu verlangen. Hierzu können Sie sich jederzeit an uns wenden. Das Recht auf Einschränkung der Verarbeitung besteht in folgenden Fällen: Wenn Sie die Richtigkeit Ihrer bei uns gespeicherten personenbezogenen Daten bestreiten, benötigen wir in der Regel Zeit, um dies zu überprüfen. Für die Dauer der Prüfung haben Sie das Recht, die Einschränkung der Verarbeitung Ihrer personenbezogenen Daten zu verlangen. Wenn die Verarbeitung Ihrer personenbezogenen Daten unrechtmäßig geschah/geschieht, können Sie statt der Löschung die Einschränkung der Datenverarbeitung verlangen. Wenn wir Ihre personenbezogenen Daten nicht mehr benötigen, Sie sie jedoch zur Ausübung, Verteidigung oder Geltendmachung von Rechtsansprüchen benötigen, haben Sie das Recht, statt der Löschung die Einschränkung der Verarbeitung Ihrer personenbezogenen Daten zu verlangen. Wenn Sie einen Widerspruch nach Art. 21 Abs. 1 DSGVO eingelegt haben, muss eine Abwägung zwischen Ihren und unseren Interessen vorgenommen werden. Solange noch nicht feststeht, wessen Interessen überwiegen, haben Sie das Recht, die Einschränkung der Verarbeitung Ihrer personenbezogenen Daten zu verlangen. Wenn Sie die Verarbeitung Ihrer personenbezogenen Daten eingeschränkt haben, dürfen diese Daten von ihrer Speicherung abgesehen nur mit Ihrer Einwilligung oder zur Geltendmachung, Ausübung oder Verteidigung von Rechtsansprüchen oder zum Schutz der Rechte einer anderen natürlichen oder juristischen Person oder aus Gründen eines wichtigen öffentlichen Interesses der Europäischen Union oder eines Mitgliedstaats verarbeitet werden.
+        </>} />
+        <Subtext variant="white" text={<>
+            <strong style={{fontWeight: "800"}}></strong><br/>
+            
+        </>} />
         </>
     )
 }
