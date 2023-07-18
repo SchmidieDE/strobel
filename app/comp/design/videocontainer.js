adbae28c6e67944860bfd650636c65c84ac4566f
SchmidieDE <41941167+SchmidieDE@users.noreply.github.com>
2023-07-18 12:42:55 +0200

    add some stuff

--- app/comp/design/videocontainer.js
+++ app/comp/design/videocontainer.js
@@ -8,10 +8,10 @@ import { IconButton } from "@mui/material";
 import { useState, useRef } from "react";
 
 
-const Videocontainer = ({linkhref, color}) => {
+const Videocontainer = ({linkhref, color, variant}) => {
 
 
-
+    
 
 
    
@@ -27,6 +27,26 @@ const Videocontainer = ({linkhref, color}) => {
             videoref.current.pause()
         } 
     }
+
+    const Switcher = () => { 
+
+        switch(variant) {
+            case "Hochkant":
+                return (
+                    <video ref={videoref} muted controls loop={!muted} autoplay style={{display: "block", marginBottom: "1rem", marginLeft: "auto", marginRight: "auto", borderRadius: "3vmin", height: "400px",  overflow: "hidden", boxShadow: "rgba(0, 0, 0, 0.1) 0px 2px 36px 0px, rgba(0, 0, 0, 0.06) 0px 0px 0px 1px"}}> 
+                        <source src={`${linkhref}`} type="video/mp4" />
+                    </video>
+                );
+            default: 
+                return (
+                    <video ref={videoref} muted controls loop={!muted} autoplay style={{display: "block", marginBottom: "1rem", marginLeft: "auto", marginRight: "auto", borderRadius: "3vmin", height: "200px",  overflow: "hidden", boxShadow: "rgba(0, 0, 0, 0.1) 0px 2px 36px 0px, rgba(0, 0, 0, 0.06) 0px 0px 0px 1px"}}> 
+                        <source src={`${linkhref}`} type="video/mp4" />
+                    </video>    
+                );
+        }
+    }
+
+
         return (
             <div style={{position: "relative", width: "90%", margin: "auto"}}>
             
@@ -37,11 +57,9 @@ const Videocontainer = ({linkhref, color}) => {
                         <PauseIcon onClick={handleMute} fontSize="large" style={{color: "white", backgroundColor: color, padding: "0.3rem",borderRadius: "1vmin", zIndex: 1}}/>
                     }
                 </IconButton>
-    
-                <video ref={videoref} muted controls loop autoplay style={{display: "block", marginBottom: "1rem", borderRadius: "3vmin", height: "200px",  overflow: "hidden", boxShadow: "rgba(0, 0, 0, 0.1) 0px 2px 36px 0px, rgba(0, 0, 0, 0.06) 0px 0px 0px 1px"}}>
-                    
-                    <source src={`${linkhref}`} type="video/mp4" />
-                </video>
+                {
+                Switcher()
+                }
             </div>)
 
 
